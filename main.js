f6af08e107e170268e3143990d26142042136f74
AlmirPaulo <ap.freelas@gmail.com>
2021-04-06 23:27:18 -0300

    first things

---
+++ main.js
@@ -0,0 +1,20 @@
+//Some Setup
+const canvas = document.querySelector('canvas')
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+const ctx = canvas.getContext('2d');
+
+
+var player = new Player(30,30)
+console.log(player.width);
+
+
+ 
+
+
+
+
+
+
---
+++ player.js
@@ -0,0 +1,6 @@
+class Player {
+    constructor(height, width) {
+        this.name = 'Polygon';
+        this.height = height;
+        this.width = width;
+}};
